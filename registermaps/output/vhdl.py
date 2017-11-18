fd73543c95920016fede132c57c82c3424d03c29
Rob Gaddi <rg@robgaddi.com>
2017-11-17 21:05:39 -0800

    Moved the VHDL AXI4-Lite template to Jinja

--- registermaps/output/vhdl.py
+++ registermaps/output/vhdl.py
@@ -663,12 +663,9 @@ class VhdlAxi4Lite(Visitor):
         addrhigh = (node.size - 1).bit_length() - 1
         datahigh = node.width - 1
         behigh = (node.width // 8) - 1
-        self.printf(self.rt('body_component'),
-            name = node.name,
-            addrhigh = addrhigh,
-            behigh = behigh,
-            datahigh = datahigh
-        )
+        self.print(jinja.get_template('vhdl-axi4lite/body_component.j2').render(
+            node = node,
+        ))
         
     def visit_MemoryMap(self, node):
         pass
